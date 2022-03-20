407aa783fea632457d20be4e91ae68bfd7d326f7
Rolando <riverarolando71@gmail.com>
2022-03-20 15:32:32 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ let nombre=document.querySelector(".nombre");
 let generar=document.querySelector(".generar");
 
 function generador(){
-    let nombres=["Carlos","Miguel","Chepe","Maria","Raquel","Josefa","Julio","Dorlen","Manuel","Pedro","Marta","Juana","Maria","Ana","Rolando","Douglas","Duglas","Alberto"]
+    let nombres=["Carlos","Miguel","José","Maria","Raquel","Josefa","Julio","Dorlen","Manuel","Pedro","Marta","Juana","Maria","Ana","Rolando","Douglas","Duglas","Alberto"]
+    let apellidos=["Rivera","Saborio","Mendez","Peña","Sousa","Hernández","Aráujo","Sandoval","Artavia","Muños","Moura","Murillo","Sotelo","Fernández","Pereira","Ortiz","Torres","Bravo"]
     for(let n=0;n<nombres.length;n++){
-    let contar=Math.round(Math.random()*n)
-
-        nombre.innerHTML=nombres[contar]
-
+        let contar=Math.round(Math.random()*n)
+        for(let a=0;a<apellidos.length;a++){
+            let contarA=Math.round(Math.random()*a)
+        
+            nombre.innerHTML=nombres[contar] +" "+ apellidos[contarA]
+        
+            }
     }
+
 }
 generar.addEventListener("click",()=>{
     generador()
